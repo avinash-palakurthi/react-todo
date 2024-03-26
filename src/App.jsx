6508b58578cf1dc7a6592e18d944f3002ef20cf9
Avinash <palakurthiavinash07@gmail.com>
2024-03-26 07:29:13 +0530

    todo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+	return (
+		<>
+			<div className="bg-black h-screen text-zinc-500  w-full  ">
+				<Navbar />
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+	return <div className=" border-b  border-zinc-500">Navbar</div>;
+};
+
+export default Navbar;
